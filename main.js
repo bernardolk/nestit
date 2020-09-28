f9910ac225d4c3af665c214ba411d9344cd6f000
bernardolk <bernardo.knackfuss@neoep.com.br>
2020-09-28 00:49:48 -0300

    implemented reduce

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ Nestit.prototype.by = function (key, label) {
       throw new "Not all objects in array have 'label' key or contains 'label' as an non-string object.";
 
    if (!label) {
-      label = key;
+      label = typeof key === "function" ? 'expression' : key;
    }
 
    if (check_value > 0) {
@@ -71,7 +71,13 @@ Nestit.prototype.by = function (key, label) {
       while (k < len) {
          if (k in O) {
             let kobject = O[k];
-            let value = kobject[key];
+            let value;
+            if (typeof key === "function") {
+               value = key(kobject, k);
+            }
+            else {
+               value = kobject[key];
+            }
             let memo_index = memo.indexOf(value);
             let ind = memo_index;
             if (memo_index === -1) {
@@ -130,13 +136,13 @@ Nestit.prototype.sum = function (field) {
          if (k in O) {
             let arr = Array.from(O[k]['values']);
             let acc_sum = new Nestit(arr).sum(field);
-            if(acc_sum instanceof Nestit){
+            if (acc_sum instanceof Nestit) {
                let res = O[k];
                O[k].values = acc_sum.Array;
                result.push(res);
             }
-            else{
-               result.push({... O[k], sum: {over: field, value: acc_sum }});
+            else {
+               result.push({ ...O[k], sum: { over: field, value: acc_sum } });
             }
          }
          k++;
@@ -157,6 +163,91 @@ Nestit.prototype.sum = function (field) {
          k++
       }
 
+      return acc;
+   }
+}
+
+
+Nestit.prototype.reduce = function (callback, initialValue, label) {
+   if (this === null) {
+      throw new TypeError('this is null or not defined.');
+   }
+
+   if (!typeof callback === "function") {
+      throw "Argument " + callback + " is not a function.";
+   }
+
+   if (initialValue === undefined) {
+      throw "No initial value provided.";
+   }
+
+   if (!label) {
+      label = "";
+   }
+
+   let O = Object(this.Array);
+   let len = O.length >>> 0;
+   let result = [];
+
+   if(len === 0){
+      return result;
+   }
+
+   let j = 0, check_value = 0, check_key = 0, check_label = 0;
+   while (j < len) {
+      if ('values' in O[j] && Array.isArray(O[j]['values']))
+         check_value++;
+      if ('key' in O[j] && typeof O[j]['key'] === "string")
+         check_key++;
+      if ('label' in O[j] && typeof O[j]['label'] === "string")
+         check_label++;
+      j++;
+   }
+
+   if (check_value > 0 && check_value < len)
+      throw  "Not all objects in array have 'values' key or contains 'values' as an non-array object.";
+
+
+   if (check_key > 0 && check_key < len)
+      throw  "Not all objects in array have 'key' key or contains 'key' as an non-string object.";
+
+   if (check_label > 0 && check_label < len)
+      throw  "Not all objects in array have 'label' key or contains 'label' as an non-string object.";
+
+
+   if (check_value > 0) {
+      let k = 0
+      while (k < len) {
+         if (k in O) {
+            let arr = Array.from(O[k]['values']);
+            let acc_sum = new Nestit(arr).reduce(...arguments);
+            if (acc_sum instanceof Nestit) {
+               O[k].values = acc_sum.Array;
+               result.push(O[k]);
+            }
+            else {
+               result.push({ ...O[k], reduce: { label: label, value: acc_sum } });
+            }
+         }
+         k++;
+      }
+      return new Nestit(result);
+   }
+   else {
+      let k = 0, acc;
+      if(typeof initialValue === "function"){
+         acc = initialValue(O[0]);
+         k++;
+      }
+      else{
+         acc = initialValue;
+      }
+      while (k < len) {
+         if (k in O) {
+            acc = callback(acc, O[k], k, O);
+         }
+         k++
+      }
       return acc;
    }
 }
@@ -182,11 +273,18 @@ Nestit.prototype.sum = function (field) {
 
 
 let bysurnamewithage = new Nestit(names_dataset)
-   .by('surname')
+   .by(x => x.surname)
    .sum('age')
    .by('gender')
    .sum('age')
    .Array;
+
+
+let reduceTest = new Nestit(names_dataset)
+   .by(x => x.surname, 'surname')
+   .reduce((acc, x) => acc + '-'+ x.name, x => x.name, "concatNames")
+   .Array;
+
 
 let j = 0;
 
